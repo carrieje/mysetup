ae128ce9e813504bacfce5b414c185113b5ecb93
Jeremy Carrier <carrieje@ensimag.fr>
2014-01-28 19:29:25 +0100

    New bashrc customization

---
+++ bashrc_extension.sh
@@ -0,0 +1,12 @@
+# MySetup configuration #
+#########################
+
+# Personal aliases
+source $MYSETUP_PATH/bash_aliases
+
+
+# GCC coloration        #
+#########################
+# Will be available from GCC 4.9
+
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01;36:quote=01'
--- setup.sh
+++ setup.sh
@@ -11,12 +11,9 @@ ln -s --backup=t $PWD/.indent.pro $HOME/.indent.pro
 
 # Bashrc customization
 echo -e "\n\n# MySetup configuration #\n#########################\n" >> ~/.bashrc
-
-# Personal bash aliases
 echo -e "# MYSETUP Path\nexport MYSETUP_PATH=$PWD" >> ~/.bashrc
-
-# Personal bash aliases
-echo -e "# Personal aliases\nsource $PWD/bash_aliases" >> ~/.bashrc
+echo -e "source \$MYSETUP_PATH/bashrc_extension.sh" >> ~/.bashrc
 
 # DONE
 echo "Here you are, mysetup is successfully installed !"
+
